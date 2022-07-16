a824ef2c9a81724d10d1f51438a88eafb08ff306
Reza Hosseini <34204580+reza6788@users.noreply.github.com>
2022-07-16 20:38:16 +0200

    add Repository Module to Register any repositories into Autofac

---
+++ Kuk.Data/Configuration/RepositoryModule.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Autofac;
+using Kuk.Data.Common;
+using Module = Autofac.Module;
+
+namespace Kuk.Data.Configuration
+{
+    public class RepositoryModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            builder.RegisterType<KukDbContext>().InstancePerLifetimeScope();
+
+            builder.RegisterGeneric(typeof(BaseRepository<>)).As(typeof(IBaseRepository<>)).InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(x => x.IsPublic && x.IsClass && x.Name.EndsWith("Repository")).AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
