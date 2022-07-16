cba10c727aa0c44e26017aa44e4361b6de6bdc84
Reza Hosseini <34204580+reza6788@users.noreply.github.com>
2022-07-16 22:11:48 +0200

    Add GetById validator in common folder in Service project to validate Id value in services

---
+++ Kuk.Services/Common/GetByIdValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Kuk.Services.Common
+{
+    public class GetByIdValidator : AbstractValidator<int>
+    {
+        public GetByIdValidator()
+        {
+            RuleFor(p => p).NotEmpty().WithMessage("ID must not be empty")
+                .GreaterThan(0).WithMessage("ID must be greater than zero");
+        }
+    }
+}
