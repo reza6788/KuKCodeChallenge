4e1ed02f003d8c5ba1940a5ca9d202c57dff44a5
Reza Hosseini <34204580+reza6788@users.noreply.github.com>
2022-07-16 23:25:57 +0200

    add Service Module to inject all Services to AutoFac

---
+++ Kuk.Services/Configuration/ServiceModule.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Autofac;
+using Kuk.Data.Configuration;
+using Module = Autofac.Module;
+
+namespace Kuk.Services.Configuration
+{
+    public class ServiceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterModule(new RepositoryModule());
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(x => x.IsPublic && x.IsClass && x.Name.EndsWith("Service")).AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+        }
+    }
+}
