bb9eec3b7ba2e8eb2d82bcff7b3446b1e51ec699
Reza Hosseini <34204580+reza6788@users.noreply.github.com>
2022-07-16 13:23:06 +0200

    Add BaseEntity class into Entitie Project

---
+++ Kuk.Entities/Common/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace Kuk.Entities.Common
+{
+    public interface IEntity
+    {
+    }
+    
+    public abstract class BaseEntity<TKey> : IEntity
+    {
+        public TKey Id { get; set; }
+    }
+
+    public abstract class BaseEntityGuid : BaseEntity<Guid>
+    {
+    }
+
+    public abstract class BaseEntityInt : BaseEntity<int>
+    {
+    }
+}
